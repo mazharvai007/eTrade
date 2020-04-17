ff516b0fa05e42beff46ec638f9b5d3f8d93ce74
swopnil <swopnil420@gmail.com>
2020-04-17 17:58:36 +0600

    Creating my first controller

--- app/routing/routes.php
+++ app/routing/routes.php
@@ -13,12 +13,20 @@ $router = new AltoRouter();
  * Using map method for the fetch routing
  * *************************************
  */
-//$router->map('GET','/about', '', 'about-us');
-//$match = $router->match();
-//
-//if ($match) {
-//    $match['target']();
-//}
-//var_dump($match);
 
+$router->setBasePath('');
 
+$router->map( 'GET', '/', '', 'home');
+$match = $router->match();
+
+if ($match) {
+
+    require_once __DIR__ . '/../controllers/BaseController.php';
+    require_once __DIR__ . '/../controllers/IndexController.php';
+
+    $index = new App\Controllers\IndexController();
+    $index->show();
+} else {
+    header($_SERVER['SERVER_PROTOCOL'] . '404 Not Found');
+    echo "Page not found";
+}
--- public/index.php
+++ public/index.php
@@ -3,5 +3,3 @@
 require_once __DIR__ . '/../bootstrap/init.php';
 
 $app_name = getenv('APP_NAME');
-
-var_dump(in_array('mod_rewrite', apache_get_modules()));
