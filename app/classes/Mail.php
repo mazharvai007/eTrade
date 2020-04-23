fb1545fff77ae645212af7b437207661b80895fe
swopnil <swopnil420@gmail.com>
2020-04-24 00:05:51 +0600

    Email sending function is done but not send the email and routing problem

--- app/classes/Mail.php
+++ app/classes/Mail.php
@@ -58,23 +58,11 @@ class Mail
      */
     public function send($data)
     {
-        $body = '
-            <!doctype html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport"
-                      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <title>Email Testing</title>
-            </head>
-            <body>
-            
-            </body>
-            </html>        
-        ';
         $this->mail->addAddress($data['to'], $data['name']);
         $this->mail->Subject = $data['subject'];
-        $this->mail->Body = $body;
+        $this->mail->Body = make($data['view'], array('data' => $data['body']));
+
+        // Mail send
+        return $this->mail->send();
     }
 }
--- resources/views/emails/welcome.php
+++ resources/views/emails/welcome.php
@@ -9,5 +9,7 @@
 </head>
 <body>
 
+<?php echo $data; ?>
+
 </body>
 </html>
