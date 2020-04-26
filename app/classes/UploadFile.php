7b20a30768cfe91e1e0d54bd8e3ebb3ffe008214
swopnil <swopnil420@gmail.com>
2020-04-26 23:32:23 +0600

    Make move method for file move to the absolute path.

--- app/classes/UploadFile.php
+++ app/classes/UploadFile.php
@@ -34,7 +34,7 @@ class UploadFile
 
         $name = strtolower(str_replace(['_', ' '], '-', $name));
         $hash = md5(microtime());
-        $ext = $this->fileExtension();
+        $ext = $this->fileExtension($file);
 
         $this->filename = "{$name}-{$hash}.{$ext}";
     }
@@ -89,6 +89,43 @@ class UploadFile
     public function path()
     {
         return $this->path;
+    }
+
+    /**
+     * Move the file to intended location
+     * @param $tmp_path
+     * @param $folder
+     * @param $file
+     * @param $new_filename
+     * @return $this|null
+     */
+
+    public function move($tmp_path, $folder, $file, $new_filename)
+    {
+        // Static obj
+        $fileObj = new static();
+        $ds = DIRECTORY_SEPARATOR;
+
+        // Set file and filename
+        $fileObj->setName($file, $new_filename);
+        $file_name = $fileObj->getName();
+
+        // Check and make folder
+        if (!is_dir($folder)) {
+            mkdir($folder, 0777, true);
+        }
+
+        // For file path
+        $fileObj->path = "{$folder}{$ds}{$file_name}";
+
+        // Absolute path
+        $absolute_path = BASE_PATH."{$ds}public{$ds}$fileObj->path";
+
+        if (move_uploaded_file($tmp_path, $absolute_path)) {
+            return $fileObj;
+        }
+
+        return null;
     }
 
 }
