6cb7b38f94ec3b54d68385d7a699b669da6cfda3
swopnil <swopnil420@gmail.com>
2020-05-15 16:50:01 +0600

    Added product quantity, image, description field and buttons of reset and save.

--- bootstrap/cache/2daaf555bffb94e852df489d30111d06c7cbf5b2.php
+++ bootstrap/cache/2daaf555bffb94e852df489d30111d06c7cbf5b2.php
@@ -52,6 +52,38 @@
                     </label>
                 </div>
             </div>
+            <div class="grid-x grid-margin-x">
+                <div class="cell small-12 medium-6 large-6">
+                    <label>Product Quantity:
+                        <select name="quantity">
+                            <option value="<?php echo e(\App\Classes\Request::old('post', 'quantity') ? : ''); ?>">
+                                <?php echo e(\App\Classes\Request::old('post', 'quantity') ? : '---Select Quantity---'); ?>
+
+                            </option>
+
+                            <?php for($i = 1; $i <= 50; $i++): ?>
+                                <option value="<?php echo e($i); ?>"><?php echo e($i); ?></option>
+                            <?php endfor; ?>
+                        </select>
+                    </label>
+                </div>
+                <div class="cell small-12 medium-6 large-6">
+                    <br>
+                    <div class="input-group">
+                        <span class="input-group-label">Product Image</span>
+                        <input class="input-group-field" type="file">
+                    </div>
+                </div>
+
+                <div class="cell small-12">
+                    <label>Product Description:
+                        <textarea name="description" placeholder="Write the product description"><?php echo e(\App\Classes\Request::old('post', 'description')); ?></textarea>
+                    </label>
+                    <input type="hidden" name="token" value="<?php echo e(\App\Classes\CSRFToken::_token()); ?>">
+                    <button class="button alert" type="reset">Reset</button>
+                    <input class="button success" type="Submit" value="Save">
+                </div>
+            </div>
         </form>
 
     </div>
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -52,6 +52,37 @@
                     </label>
                 </div>
             </div>
+            <div class="grid-x grid-margin-x">
+                <div class="cell small-12 medium-6 large-6">
+                    <label>Product Quantity:
+                        <select name="quantity">
+                            <option value="{{ \App\Classes\Request::old('post', 'quantity') ? : '' }}">
+                                {{ \App\Classes\Request::old('post', 'quantity') ? : '---Select Quantity---' }}
+                            </option>
+
+                            @for($i = 1; $i <= 50; $i++)
+                                <option value="{{ $i }}">{{ $i }}</option>
+                            @endfor
+                        </select>
+                    </label>
+                </div>
+                <div class="cell small-12 medium-6 large-6">
+                    <br>
+                    <div class="input-group">
+                        <span class="input-group-label">Product Image</span>
+                        <input class="input-group-field" type="file">
+                    </div>
+                </div>
+
+                <div class="cell small-12">
+                    <label>Product Description:
+                        <textarea name="description" placeholder="Write the product description">{{ \App\Classes\Request::old('post', 'description')}}</textarea>
+                    </label>
+                    <input type="hidden" name="token" value="{{ \App\Classes\CSRFToken::_token() }}">
+                    <button class="button alert" type="reset">Reset</button>
+                    <input class="button success" type="Submit" value="Save">
+                </div>
+            </div>
         </form>
 
     </div>
